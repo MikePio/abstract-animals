2d61525d7a976d4c2fbc989d7d0dcad0b954e2b1
MikePio <michelepiopilla@gmail.com>
2023-09-25 02:41:56 +0200

    Instantiated and called the faiVolare() and faiNuotare() methods in the Main file (using instanceof)

--- src/org/java/animals/Main.java
+++ src/org/java/animals/Main.java
@@ -6,6 +6,9 @@
 
 package org.java.animals;
 
+import org.java.animals.interfaces.INuotante;
+import org.java.animals.interfaces.IVolante;
+
 public class Main {
 	public static void main(String[] args) {
 		// System.out.println("Hello world");
@@ -40,19 +43,60 @@ public class Main {
 		//* richiamato metodo dell'interfaccia
 		// passerotto1.vola();
 
-		System.out.println("\n--------------------\n");
-		System.out.println(aquila1);
-		aquila1.vola();
-		System.out.println("\n--------------------\n");
-		System.out.println(cane1);
-		System.out.println("\n--------------------\n");
-		System.out.println(delfino1);
-		delfino1.nuota();
-		System.out.println("\n--------------------\n");
-		System.out.println(passerotto1);
-		passerotto1.vola();
-		System.out.println("\n--------------------\n");
+		// senza ciclo for
+		// System.out.println("\n--------------------\n");
+		// System.out.println(aquila1);
+		// aquila1.vola();
+		// System.out.println("\n--------------------\n");
+		// System.out.println(cane1);
+		// System.out.println("\n--------------------\n");
+		// System.out.println(delfino1);
+		// delfino1.nuota();
+		// System.out.println("\n--------------------\n");
+		// System.out.println(passerotto1);
+		// passerotto1.vola();
+		// System.out.println("\n--------------------\n");
 
+		// con ciclo for ed array
+		Animale[] animali = new Animale[4];
+		animali[0] = aquila1;
+		animali[1] = cane1;
+		animali[2] = delfino1;
+		animali[3] = passerotto1;
 
+		for(int i = 0; i < animali.length; i++){
+		
+			Animale animale = animali[i];
+
+			System.out.println("\n--------------------\n");
+			System.out.println(animale.getClass().getSimpleName() + "\n");
+			animale.dorme();
+			animale.verso();
+			animale.mangia();
+			
+			//se è presente l'interfaccia INuotante nella classe stampa questo
+			if (animale instanceof INuotante){
+				INuotante aNuotante = (INuotante) animale;
+				
+				faiNuotare(aNuotante);
+			}
+			
+			//se è presente l'interfaccia IVolante nella classe stampa questo
+			if (animale instanceof IVolante){
+				IVolante aVolante = (IVolante) animale;
+
+				faiVolare(aVolante);
+			}	
+		
+		}
+		
+	}
+
+	static void faiVolare(IVolante animaleVolante){
+		animaleVolante.vola();
+	}
+
+	static void faiNuotare(INuotante animaleNuotante){
+		animaleNuotante.nuota();
 	}
 }
